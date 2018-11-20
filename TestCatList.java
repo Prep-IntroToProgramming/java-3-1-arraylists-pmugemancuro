f54c3e11abc49871fdb660947f8160aee38faa7f
PM <19mugemancurop@flintridgeprep.org>
2018-11-19 21:44:44 -0800

    New changes.

---
+++ TestCatList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TestCatList{
+   public static void main (String[] args){
+       ArrayList <Cat> CatList = new ArrayList <Cat>();
+       for (int i=0; i<4; i++){
+           CatList.add (new Cat());
+           Cat.setName = 
+       }
+   }
+}
