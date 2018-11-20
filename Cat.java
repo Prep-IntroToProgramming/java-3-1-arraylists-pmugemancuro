db7ffe16e62b68589514601dad29a5f18064dc4f
PM <19mugemancurop@flintridgeprep.org>
2018-11-19 20:44:13 -0800

    First Commit. Assignment part 1 done.

--- Cat.java
+++ Cat.java
@@ -1,3 +1,31 @@
 
 public class Cat {
+    private String name;
+    private double weight;
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public void setWeight(double newWeight){
+        weight = newWeight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public Cat(){   
+    }
+
+    public Cat(String catName){
+        catName = name;
+    }
+    
+    public void meow(){
+        System.out.print ("Meow!");
+    }
 }
